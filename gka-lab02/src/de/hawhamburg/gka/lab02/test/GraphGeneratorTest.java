7969705369532e032c9d39771522af80372f2de4
blurryroots <blurryroots@posteo.de>
2014-12-03 21:27:46 +0100

    graph generator test

--- gka-lab02/src/de/hawhamburg/gka/lab02/test/GraphGeneratorTest.java
+++ gka-lab02/src/de/hawhamburg/gka/lab02/test/GraphGeneratorTest.java
@@ -1,6 +1,8 @@
 package de.hawhamburg.gka.lab02.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 
 import org.jgrapht.Graph;
 import org.junit.After;
@@ -11,7 +13,14 @@ import de.hawhamburg.gka.common.CustomEdge;
 import de.hawhamburg.gka.lab02.GraphGenerator;
 
 public class GraphGeneratorTest {
-
+	private
+	boolean do800 = false;
+	private
+	boolean do2500 = false;
+	
+	private
+	long t;
+	
 	@Before
 	public void setUp () throws Exception {
 	}
@@ -22,20 +31,44 @@ public class GraphGeneratorTest {
 
 	@Test
 	public void test800 () {
-		final int vc = 800;
-//		final int ec = vc * (vc - 1);
-		final int ec = 300000;
-		Graph<String, CustomEdge> graph = GraphGenerator.generateDirected (vc, ec, 42, 1);
-
+		if (do800) {
+			final int vc = 800;
+	//		final int ec = vc * (vc - 1);
+			final int ec = 300000;
+			Graph<String, CustomEdge> graph = GraphGenerator.generateDirected (vc, ec, 42, 1);
+		}
+		
 		assertTrue (true);
+	}
+	
+	@Test
+	public void testKeks () {
+		this.startTimer ();
+		GraphGenerator.generateDirected (800, 100000, 42, 1);
+		System.out.println ("time: " + this.stopTimer ());
 	}
 
 	@Test
 	public void test2500 () {
-		final int vc = 2500;
-		final int ec = 2000000;
-		Graph<String, CustomEdge> graph = GraphGenerator.generateDirected (vc, ec, 1337, 42);
+		if (do2500) {
+			final int vc = 2500;
+			final int ec = 2000000;
+			Graph<String, CustomEdge> graph = GraphGenerator.generateDirected (vc, ec, 1337, 42);
+		}
 		
 		assertTrue (true);
+	}
+	
+	private
+	void startTimer () {
+		this.t = System.currentTimeMillis ();
+	}
+	
+	private
+	double stopTimer () {
+		long c = System.currentTimeMillis () - this.t;
+		this.t = 0;
+		
+		return (double)c / 1000.0;
 	}
 }
