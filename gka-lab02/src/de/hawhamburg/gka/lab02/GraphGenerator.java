3f830bc9db907358e28b8f0b9858a8c8d8ad4366
blurryroots <blurryroots@posteo.de>
2014-12-04 00:14:36 +0100

    rewrote algorithm for graph generation

--- gka-lab02/src/de/hawhamburg/gka/lab02/GraphGenerator.java
+++ gka-lab02/src/de/hawhamburg/gka/lab02/GraphGenerator.java
@@ -1,6 +1,5 @@
 package de.hawhamburg.gka.lab02;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -11,14 +10,18 @@ import org.jgrapht.graph.DefaultDirectedGraph;
 import de.hawhamburg.gka.common.CustomEdge;
 
 public class GraphGenerator {
-	public static
-	Graph<String, CustomEdge> generateDirected (int verticesCount, int edgeCount, int maxWeight, int minWeight) {
-		Random r = new Random (1337);
-		
+	private
+	Random r;
+	
+	public
+	GraphGenerator (int seed) {
+		this.r = new Random (seed);
+	}
+	
+	public
+	Graph<String, CustomEdge> generateDirected (int verticesCount, int edgeCount, int maxWeight, int minWeight) {		
 		Graph<String, CustomEdge> graph = 
 			new DefaultDirectedGraph<String, CustomEdge> (CustomEdge.class);
-		Graph<String, CustomEdge> completeGraph = 
-				new DefaultDirectedGraph<String, CustomEdge> (CustomEdge.class);
 		
 		List<String> vertices = new LinkedList<String> ();		
 		for (int i = 0; i < verticesCount; ++i) {
@@ -27,44 +30,22 @@ public class GraphGenerator {
 		
 		for (String vertex : vertices) {
 			graph.addVertex (vertex);
-			completeGraph.addVertex (vertex);
 		}
 		
-		for (int itc = 0; itc < verticesCount; ++itc) {
-			for (int vi = 0; vi < verticesCount; ++vi) {
-				StringBuilder b = new StringBuilder ();
-				b.append ("path").append (itc).append (vi);
-				if (vi == itc) {
-					continue;
-				}
-				completeGraph.addEdge (
-					vertices.get (itc),
-					vertices.get (vi),
-					new CustomEdge (b.toString (), r.nextInt (maxWeight) + minWeight)
-				);
-			}
-		}
-		
-//		List<CustomEdge> edges = new LinkedList<CustomEdge> ();
-//		for (int i = 0; i < edgeCount; ++i) {
-//			edges.add (new CustomEdge ("path" + i, r.nextInt (maxWeight) + minWeight));
-//		}
-//		for (CustomEdge edge : edges) {
-//			String v1, v2;
-//			do {
-//				v1 = vertices.get (r.nextInt (verticesCount));
-//				v2 = vertices.get (r.nextInt (verticesCount));
-//			} while (v1.equals (v2) || graph.containsEdge (v1, v2));
-//			
-//			graph.addEdge (v1, v2, edge);
-//		}
-
-		final int maxEdgeCount = verticesCount * (verticesCount - 1) / 2;
-		List<CustomEdge> possibleEdges = new ArrayList<> (completeGraph.edgeSet ());
+		List<CustomEdge> edges = new LinkedList<CustomEdge> ();
 		for (int i = 0; i < edgeCount; ++i) {
-			int edgeIndex = r.nextInt (possibleEdges.size ());
-			CustomEdge edge = possibleEdges.remove (edgeIndex);
-			graph.addEdge (edge.getSource (), edge.getTarget (), edge);
+			edges.add (new CustomEdge ("path" + i, r.nextInt (maxWeight) + minWeight));
+		}
+		while (! edges.isEmpty ()) {
+			String v1 = vertices.get (r.nextInt (verticesCount));
+			String v2 = vertices.get (r.nextInt (verticesCount));
+
+			// zweiten knoten im falle von kollision aufsteigen ermitteln	
+			while (v1.equals (v2) || graph.containsEdge (v1, v2)) {
+				v2 = vertices.get (r.nextInt (verticesCount));
+			} 
+			
+			graph.addEdge (v1, v2, edges.remove (0));
 		}
 		
 		return graph;
