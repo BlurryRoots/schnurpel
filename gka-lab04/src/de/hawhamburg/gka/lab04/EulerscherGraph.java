96e89347a4709d6d7c02ec5d478f57efec1f6c12
Varinor <lynxdeluxe@hotmail.de>
2015-01-13 20:17:11 +0100

    added gka-lab04

---
+++ gka-lab04/src/de/hawhamburg/gka/lab04/EulerscherGraph.java
@@ -0,0 +1,18 @@
+package de.hawhamburg.gka.lab04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jgrapht.UndirectedGraph;
+
+import de.hawhamburg.gka.common.CustomEdge;
+
+public class EulerscherGraph {
+	
+	public UndirectedGraph<String, CustomEdge> eulerscherGraph(UndirectedGraph<String, CustomEdge> graph){
+		
+		return null;
+		
+	}
+	
+}
