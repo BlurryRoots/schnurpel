e850a3efd39ee8583e82887be4a9c4c954648bea
blurryroots <blurryroots@posteo.de>
2015-01-14 10:02:52 +0100

    generator algorithm and classpath

--- gka-common/src/de/hawhamburg/gka/common/Generator.java
+++ gka-common/src/de/hawhamburg/gka/common/Generator.java
@@ -49,19 +49,30 @@ class Generator {
 		}
 		
 		// create a list of unweighted edges (Lue)
-		List<CustomEdge> unweightedEdges = new LinkedList<CustomEdge> (graph.edgeSet ());
+		List<CustomEdge> unweightedEdges = new LinkedList<> (graph.edgeSet ());
 		
 		// do as long as (Lue) is not empty
 		while (! unweightedEdges.isEmpty ()) {
-			// select random edge (E) and its vertices (Va & Vb) and assign a random weight (W)
-			int index = 
+			// select random edge (E) and its vertices (Va & Vb)
+			int index = this.random.nextInt (unweightedEdges.size ());
+			// and assign a random weight (W)
 			// remove (E) from (Lue)
+			CustomEdge edge = unweightedEdges.remove (index);
+			edge.setCost (this.random.nextInt (maxWeight - minWeight) + minWeight);
 			// now select all vertices which are not connected to this edge and put them into a list (L)
+			List<String> remainingVertices = new LinkedList<> (graph.vertexSet ());
+			remainingVertices.remove (edge.getSource ());
+			remainingVertices.remove (edge.getTarget ());
+			
 			// for each vertex (V) in this list (L)
+			while (! remainingVertices.isEmpty ()) {
+				String v = remainingVertices.remove (0);
 				// select edge (E1) from current (V) to (Va) and assign w(E1) a random weight rand(W) + 1
+				CustomEdge e1 = graph.getEdge (edge.getSource (), v);
 				// remove (E1) from (Lue)
-				// select edge (E2) from current (V) to (Vb) and assign w(E2) equal to W - w(E1)
+				// select edge (E2) from current (V) to (Vb) and assign w(E2) ehttps://dl.dropboxusercontent.com/u/1499338/hair.zipqual to W - w(E1)
 				// reomve (E2) from (Lue)
+			}
 		}
 		
 		return graph;
