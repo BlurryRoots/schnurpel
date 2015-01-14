4fa2b0c01267495efc1ad792e6f4b8356ef0fc80
blurryroots <blurryroots@posteo.de>
2015-01-14 22:17:39 +0100

    furter integration stuff

--- gka-lab04/src/de/hawhamburg/gka/lab04/test/NearestNeighbourIntegrationTest.java
+++ gka-lab04/src/de/hawhamburg/gka/lab04/test/NearestNeighbourIntegrationTest.java
@@ -2,6 +2,8 @@ package de.hawhamburg.gka.lab04.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
 import org.jgrapht.Graph;
 import org.junit.After;
 import org.junit.Before;
@@ -29,6 +31,7 @@ class NearestNeighbourIntegrationTest {
 		final int SEED = 1337;
 		final int WEIGHT = 123;
 		final int VCOUNT = 24;
+		final String START = "v1";
 		
 		Graph<String, CustomEdge> problem = new Generator ()
 			.setSeed (SEED)
@@ -37,15 +40,28 @@ class NearestNeighbourIntegrationTest {
 			.build ();
 		NearestNeighbour nn = new NearestNeighbour (problem);
 		
-		Graph<String, CustomEdge> tour = nn.getTour ("v1");
+		Graph<String, CustomEdge> tour = nn.getTour (START);
 		assertNotNull (tour);
 		assertEquals (VCOUNT, tour.vertexSet ().size ());
 		assertEquals (VCOUNT, tour.edgeSet ().size ());
 		
+		Set<CustomEdge> startEdges = tour.edgesOf (START);
+
 		// TODO: calc min span tree weight
 		// TODO: calc double min span tree weight
 		// TODO: calc total weight of tour
 		// TODO: calc ration between result and best/worst case
 	}
 
+	public
+	String walkToNextNode (String from, Graph<String, CustomEdge> in) {
+		Set<CustomEdge> edges = in.edgesOf (from);
+		assertNotNull (edges);
+		CustomEdge edge = edges.iterator ().next ();
+		assertNotNull (edge);
+		
+		return (from.equals (edge.getSource ()))
+			? edge.getTarget ()
+			: edge.getSource ();
+	}
 }
