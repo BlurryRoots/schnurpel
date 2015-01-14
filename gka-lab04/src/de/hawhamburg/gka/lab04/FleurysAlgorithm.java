df432bb0befef8be48799f706bb805a0a9ae9121
Varinor <lynxdeluxe@hotmail.de>
2015-01-14 23:42:11 +0100

    added comments

--- gka-lab04/src/de/hawhamburg/gka/lab04/FleurysAlgorithm.java
+++ gka-lab04/src/de/hawhamburg/gka/lab04/FleurysAlgorithm.java
@@ -2,7 +2,6 @@ package de.hawhamburg.gka.lab04;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -15,41 +14,44 @@ import de.hawhamburg.gka.common.CustomEdge;
 
 public class FleurysAlgorithm {
 	
-	public List fleurysAlgorithm(UndirectedGraph<String, CustomEdge> graph, String startingpoint){
+	public List<String> fleurysAlgorithm(UndirectedGraph<String, CustomEdge> graph, String startingpoint){
 		
-		List<String> verticiesRoute = new ArrayList<String>();
+		List<String> verticiesRoute = new ArrayList<String>();  		//returnwert liste von vertexes, wie sie nacheinander abgelaufen werden
 		String nextVertex = startingpoint;
-		boolean checkForBridges = true;
-		boolean graphAllreadyVisited = true;
+		boolean checkForBridges = true;									//nachdem eine kante gegangen ist, wird diese danach gelösch, es müssen danach denoch alle kanten ablaufbar bleiben
+
 		CustomEdge edge = new CustomEdge();
 		  
-		while(graph.vertexSet().size() > 1){	
+		while(graph.vertexSet().size() > 1){							//der Algorithmus ist beendet, sobald alle Kanten abgelaufen sind, ich lösche im Algorithmus auch die Knoten, die keine Kanten mehr haben
+																		//wenn also nur noch ein Knoten vorhanden ist, der Anfangs- / Zielknote, dann kann die Schleife verlassen werden.
 			
-			LinkedList edgesList = new LinkedList<CustomEdge>(graph.edgesOf(nextVertex));
+			LinkedList<CustomEdge> edgesList = new LinkedList<CustomEdge>(graph.edgesOf(nextVertex)); //hole alle dem Knoten zugehörigen Kanten  
 			
-			//Choose an edge which does not disconnect the graph after removed
+			//Choose an edge which does not disconnect the graph after it is removed
 			while(checkForBridges){
-				
-				edge = (CustomEdge) edgesList.getFirst();				
-				
-				if(checkForBridges = edgeIsBridgeEdge(graph, edge))
-					edgesList.removeFirst();
 
+				edge = (CustomEdge) edgesList.getFirst();				//nehme eine der Kanten aus der Liste		
+				
+				if(checkForBridges = edgeIsBridgeEdge(graph, edge)) 	//falls es eine Brückenkante ist, entferne die Kante aus der Liste der Kanten 
+					edgesList.removeFirst();
 			}
-			
-			verticiesRoute.add(graph.getEdgeSource(edge).toString());
+			//---------------------------------------------------------------------
+			//System.out.println(edge);			
+			verticiesRoute.add(graph.getEdgeSource(edge).toString()); 	//füge der Route den Anfangsknoten der Kante, die gelaufen werden soll, hinzu
 	
-			nextVertex = graph.getEdgeTarget(edge).toString();
-			
-			graph.removeEdge(edge);
+			nextVertex = graph.getEdgeTarget(edge).toString(); 			//mache "nextVertex" zum nächsten Knoten, der betrachtet werden soll, für den nächsten durchlauf
+		
+			Set<CustomEdge> numOfEdges = new HashSet<CustomEdge>();
+						
+			graph.removeEdge(edge);										//entferne die genutze Kante
 		
 			//has source vertex still edges, if not remove it
-			if((graph.edgesOf(graph.getEdgeSource(edge))).size() == 0)
+			if((numOfEdges).size() == 0) 	//Falls der verlassene Knoten keine Kante mehr haben sollte, lösche auch diesen
 				graph.removeVertex(graph.getEdgeSource(edge));
 		
 		}
 		
-		verticiesRoute.add(nextVertex);
+		verticiesRoute.add(nextVertex);									//der letzte verbleibende Knoten wird der Liste hinzugefügt
 		
 		return verticiesRoute;
 	}
@@ -71,17 +73,14 @@ public class FleurysAlgorithm {
 	}
 */
 	
-	private boolean edgeIsBridgeEdge(UndirectedGraph graph, CustomEdge edge) {
+	private boolean edgeIsBridgeEdge(UndirectedGraph<String, CustomEdge> graph, CustomEdge edge) {
 		
-		Graph graphCopy = graph;
+		Graph<String, CustomEdge> graphCopy = graph;
 		
 		graphCopy.removeEdge(edge);
 		
-		ConnectivityInspector<String,CustomEdge> newGraph = new ConnectivityInspector<String, CustomEdge>((UndirectedGraph<String, CustomEdge>) graph);
-		
-		if(newGraph.isGraphConnected())
-			return false;
-		
-		return true;
+		ConnectivityInspector<String,CustomEdge> newGraph = new ConnectivityInspector<String, CustomEdge>((UndirectedGraph<String, CustomEdge>) graphCopy);
+	
+		return newGraph.isGraphConnected();
 	}
 }
