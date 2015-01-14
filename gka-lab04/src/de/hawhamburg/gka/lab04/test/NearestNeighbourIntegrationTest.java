a0e459f2a0a2afa30eb7b4c79102c5d1e9a80879
Varinor <lynxdeluxe@hotmail.de>
2015-01-15 00:53:10 +0100

    finished open toDo s

--- gka-lab04/src/de/hawhamburg/gka/lab04/test/NearestNeighbourIntegrationTest.java
+++ gka-lab04/src/de/hawhamburg/gka/lab04/test/NearestNeighbourIntegrationTest.java
@@ -1,16 +1,20 @@
 package de.hawhamburg.gka.lab04.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import org.jgrapht.Graph;
+import org.jgrapht.UndirectedGraph;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import de.hawhamburg.gka.common.CustomEdge;
 import de.hawhamburg.gka.common.Generator;
+import de.hawhamburg.gka.lab04.MinSpanTree;
 import de.hawhamburg.gka.lab04.NearestNeighbour;
 
 public
@@ -48,9 +52,49 @@ class NearestNeighbourIntegrationTest {
 		Set<CustomEdge> startEdges = tour.edgesOf (START);
 
 		// TODO: calc min span tree weight
+		MinSpanTree mst = new MinSpanTree ((UndirectedGraph<String, CustomEdge>) problem);
+		Graph<String, CustomEdge> minSpanTree = mst.getMinimumSpanTree();
+		
+		Set<CustomEdge> edgesSpanTreeSet = minSpanTree.edgeSet();
+		
+		CustomEdge spanTreeEdge = null;
+		int spanTreeWeight = 0;
+		
+		Iterator<CustomEdge> spanTreeIt = edgesSpanTreeSet.iterator();
+		
+		while(spanTreeIt.hasNext()){
+		
+			spanTreeEdge = spanTreeIt.next();
+			spanTreeWeight += spanTreeEdge.getCost();
+		}
+	
 		// TODO: calc double min span tree weight
+			int doubleSpanTreeWeight = spanTreeWeight * 2;
+			
 		// TODO: calc total weight of tour
+			Graph<String, CustomEdge> getTour = nn.getTour(START);
+			
+			Set<CustomEdge> edgesTourSet = getTour.edgeSet();
+			
+			Iterator<CustomEdge> tourIt = edgesTourSet.iterator();
+			
+			CustomEdge tourEdge = null;
+			int tourWeight = 0;
+			
+			while(tourIt.hasNext()){
+				
+				tourEdge = tourIt.next();
+				tourWeight += tourEdge.getCost();
+			}
+
+			System.out.println("spanTreeWeight: " + spanTreeWeight);
+			System.out.println("doubleSpanTreeWeight: " + doubleSpanTreeWeight);
+			System.out.println("tourWeight: " + tourWeight);
+
 		// TODO: calc ration between result and best/worst case
+			
+			assertEquals(true, tourWeight > spanTreeWeight && tourWeight < doubleSpanTreeWeight );
+			
 	}
 
 	public
